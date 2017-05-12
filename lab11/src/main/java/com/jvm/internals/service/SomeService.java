a880a663108dacebeb73503bbcc0d46416925745
Mateusz Zweigert <mateuszzweigert@gmail.com>
2017-05-12 09:09:37 +0200

    add missing dependencies in poms and missing service

---
+++ lab11/src/main/java/com/jvm/internals/service/SomeService.java
@@ -0,0 +1,15 @@
+package com.jvm.internals.service;
+
+public class SomeService {
+
+	private int exponent = 2;
+
+	public int getExponent() {
+		return exponent;
+	}
+
+	public void setExponent(int exponent) {
+		this.exponent = exponent;
+	}
+
+}
